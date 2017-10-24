f72560c277bd4a00463e04063e45e83f2946064e
Mark Dewey <mark.t.dewey@gmail.com>
2017-10-24 14:25:23 -0400

    added rentals to home page, needs styling a little bit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,16 @@ var request = require('request')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index');
+  let _location = {
+    term: "tampa",
+    city: "St Petersburg"
+  }
+  let _url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20craigslist.search%20where%20location%3D%22"+_location.term+"%22%20and%20type%3D%22rtr%22%20and%20query%3D%22pinellas%20apartments%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+  request.get(_url, (error, response, body) => {
+    let _json = JSON.parse(body);
+    let _apartments = _json.query.results.RDF.item;
+    res.render('index', { title: 'Premiere Rental Properties', data: _apartments, city: _location.city });
+  });
  });
 
 
